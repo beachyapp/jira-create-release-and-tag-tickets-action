13265bfd9204f992739958e2e4d6fd88e2a5b276
fathi <fathiabdoon@gmail.com>
2022-04-07 16:35:01 +1000

    init

---
+++ README.md
@@ -0,0 +1,61 @@
+# Create new version in JIRA and tag issues
+
+This action creates a new version in JIRA, and tags all the issues in with the new version by issue keys
+
+## Inputs
+
+## `tag`
+
+**Required** The new fix version you want to create in JIRA. It's usually reflective of the tag name in git.
+
+## `description`
+
+Description of the new fix version.
+
+## `issue-keys`
+
+An array of issue keys you want to tag with the new fix version.
+
+## `jira-user-email`
+
+**Required** JIRA user email
+
+## `jira-api-token`
+
+**Required** JIRA API token
+
+## `jira-base-url`
+
+**Required** JIRA base URL
+
+## Output
+
+## `notes`
+
+Release notes grouped by issue status (similar to JIRA release notes)
+
+## Example usage
+
+```
+- name: Find JIRA issue keys by commits
+  id: issue-key-from-commits
+  uses: beachyapp/jira-get-issue-key-action@v0.1
+  ....
+  ....
+- name: Create JIRA fix version and tag issue tickets
+  id: jira-version
+  uses: beachyapp/create-release-and-tag-tickets-action@v0.1
+  with:
+    tag: "v1.0.1"
+    description: Created by ${{ github.actor }}
+    issue-keys: ${{steps.issue-key-from-commits.outputs.keys}}
+    jira-user-email: ${{ secrets.JIRA_USER_EMAIL }}
+    jira-api-token: ${{ secrets.JIRA_API_TOKEN }}
+    jira-base-url: ${{ secrets.JIRA_BASE_URL }}
+- name: Print Release Notes`'
+  run: echo ${{steps.jira-version.outputs.notes}}
+```
+
+## NOTE:
+
+This assumes all tickets belong to the same project. For example, you can not have an array of issue tickets from multiple projects. Release versions in JIRA are created per project basis. 
---
+++ index.js
@@ -0,0 +1,108 @@
+const core = require('@actions/core');
+var JiraApi = require('jira-client');
+
+try {
+  const newVersion = core.getInput('tag');
+  const description = core.getInput('description');
+  let issueKeys = core.getInput('issue-keys');
+  const jiraBaseUrl = core.getInput('jira-base-url');
+  const jiraUserEmail = core.getInput('jira-user-email');
+  const jiraApiToken = core.getInput('jira-api-token');
+  const jira = new JiraApi({
+    protocol: 'https',
+    host: jiraBaseUrl.replace(/(^\w+:|^)\/\//, ''),
+    username: jiraUserEmail,
+    password: jiraApiToken,
+    apiVersion: '3',
+    strictSSL: true
+  });
+
+  if(!issueKeys || issueKeys.length === 0 || JSON.parse(issueKeys).length == 0)
+    throw 'No issueKeys found';
+  
+  issueKeys = JSON.parse(issueKeys);
+
+  async function setVersion(projectId) {
+    let versionId = 0;
+    // get all versions - to check if the new version exists
+    const versions = await jira.getVersions(projectId);
+    await versions.map(v => { 
+      if(v.name == newVersion)
+        versionId = v.id;
+    });
+    // if the new version does not exist, create one
+    if(versionId == 0){
+      const body = {
+        "name": newVersion, 
+        "description": description, 
+        "projectId": projectId
+      };
+      console.info(body)
+      const newVersions = await jira.createVersion(body);
+      versionId = newVersions.id;
+    }
+  }
+
+  async function getIssues(issueKeys){
+    issues = [];
+    for (let i = 0; i < issueKeys.length; i++) {
+      try {
+        const issue = await jira.findIssue(issueKeys[i]);
+        await issues.push(issue);
+      } catch(e) {
+        console.log(e.message);
+      }
+    }
+    return issues;
+  }
+
+  async function updateIssuesWithFixVersion(issues){
+    const updateField = {
+      update: { fixVersions: [ {"add" : {"name" : newVersion}} ] 
+    }};
+    for (let i = 0; i < issues.length; i++) {
+      await jira.updateIssue(issues[i].id, updateField);
+    }
+  }
+
+  async function createReleaseNote(issues){
+    let releaseNoteMap = {};
+    let releaseNote = [];
+    // group all tickets by ticket status
+    for (let i = 0; i < issues.length; i++) {
+      let list = releaseNoteMap[issues[i].fields.issuetype.name];
+      if(!list) list = [];
+      await list.push(`[${issues[i].key}] ${issues[i].fields.summary}`);
+      releaseNoteMap[issues[i].fields.issuetype.name] = list;
+    }
+    // prettify
+    releaseNote.push("");
+    releaseNote.push(`# RELEASE NOTES / PROJECT [${issues[0].fields.project.name.toUpperCase()}] / VERSION [${newVersion.toUpperCase()}]`);
+    for (var key in releaseNoteMap) {
+      releaseNote.push("");
+      releaseNote.push(`### ${key.toUpperCase()}`);
+      releaseNote.push(releaseNoteMap[key].join("\\n"));
+    }
+    releaseNote.push("");
+    return releaseNote.join("\\n");
+  }
+
+  if(!issueKeys || issueKeys.length == 0)
+    throw 'No issueKeys found';
+
+  // make sure keys are unique
+  issueKeys = [...new Set(issueKeys)];
+
+  getIssues(issueKeys).then(async issues => {
+    await setVersion(issues[0].fields.project.id);
+    await updateIssuesWithFixVersion(issues);
+    await createReleaseNote(issues).then(releaseNotes => {
+      releaseNotes = releaseNotes.replace(/"/g, '\\\"').replace(/'/g, "`");
+      console.info(releaseNotes);
+      core.setOutput("notes", releaseNotes);
+    });
+  });
+
+} catch (error) {
+  core.setFailed(error);
+}
